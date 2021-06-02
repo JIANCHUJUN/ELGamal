d0080546291ad354a067be40ad31bca5001e01d6
JiachengYe <yejc@craiditx>
2021-06-02 15:34:57 +0800

    commit

---
+++ build/CMakeFiles/ELGamal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ELGamal.dir/main.cpp.o"
+  "ELGamal"
+  "ELGamal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ELGamal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
